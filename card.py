a6222e3e11587b8202cfb06e5f798bd9ed558249
JeQuavionisTheIV <epersa@pcl.com>
2024-03-14 10:08:18 -0600

    Added test.py and error checking to Card
    Added seperate file location where testing can be done for the program. Might make it more modular with test_card.py etc. in the future. Also added some exception handling inside of the card class

--- card.py
+++ card.py
@@ -1,9 +1,18 @@
 import unittest
 
 class Card:
+    valid_ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    valid_suits = ['H','C','S','D']
     def __init__(self, rank, suit):
+        
+        if rank not in self.valid_ranks:
+            raise ValueError("Invalid rank.")
+        if suit not in self.valid_suits:
+            raise ValueError("Invalid suit.")
+
         self._rank = rank
         self._suit = suit
+        
 
     def __str__(self):
         return str(self._rank)+ str(self._suit)
@@ -13,10 +22,7 @@ class Card:
     
     def show_suit(self):
         return str(self._suit)
-    
-class testCard(unittest.TestCase):
 
-    def
 
 
 # example card usage
