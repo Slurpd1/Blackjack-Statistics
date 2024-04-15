f1fcf1b171c8e2b8ef08a78f453c2b9c93abdf67
Slurpd1 <104402459+Slurpd1@users.noreply.github.com>
2024-04-14 18:59:09 -0600

    simple_game complete
    User can now play a simple game via. terminal. This is mostly to rule out bugs and try to figure things out. Will keep testing until bugs are etched out

--- simple_game.py
+++ simple_game.py
@@ -10,6 +10,8 @@ def play_blackjack():
     shoe.shuffle()
 
     while True:
+        blackjack = False
+        # currently the shoe WILL EMPTY OUT. Check before playing
         # Playing a basic blackjack game with dealer
         player.add_card(shoe.deal_card())
         player.add_card(shoe.deal_card())
@@ -18,16 +20,60 @@ def play_blackjack():
         dealer.add_card(shoe.deal_card())
 
         dealer_upcard = dealer.show_upcard()
-        print("Dealer's upcard:", dealer.show_upcard())  # Display only the first card (upcard)
 
-        decision = input(f'Dealer has {dealer_upcard}. Your hand totals to {player.total_value()}, Would you like to hit or stand? (H,S)')
+        if player.total_value() == 21:
+            blackjack = True
 
-        if decision == 'H':
-            # need to pull a card from the shoe
+        if not blackjack:
+            decision = input(f"Dealer: {dealer_upcard}\nYour hand: {', '.join(player.display_hand())}\nYour total: {player.total_value()}\nHit (H), Stand(S), or Double Down(D)?")
+
+
+
+        while player.total_value() < 21:
+            if player.total_value() > 21:
+                break
+            if player.total_value == 21:
+                break
+            if decision.upper() == 'H':
+                # need to pull a card from the shoe
+                dealt_card = shoe.deal_card()
+                player.add_card(dealt_card)
+                if int(player.total_value()) == 21:
+                    break
+                if player.total_value() > 21:
+                    break
+                decision = input(f"\nDealer: {dealer_upcard}\nYour hand: {', '.join(player.display_hand())}\nYour total: {player.total_value()}\nHit (H), or Stand(S)?")
+            if decision.upper() == 'D':
+                dealt_card = shoe.deal_card()
+                player.add_card(dealt_card)
+                break
+            if decision.upper() == 'S':
+                break
+
+        while dealer.total_value() < 17 and not blackjack:
             dealt_card = shoe.deal_card()
-            player.add_card(dealt_card)
-            decision = input(f'Dealer has {dealer_upcard}. Your hand totals to {player.total_value()}, Would you like to hit or stand? (H,S)')
+            dealer.add_card(dealt_card)
+            if dealer.total_value() > 21:
+                break 
 
+        if dealer.total_value() > 21:
+            print(f'\nDealer has {', '.join(dealer.display_hand())}\tTotal {dealer.total_value()}\nYou have {', '.join(player.display_hand())}\t  Total {player.total_value()}\n You win!')
+            print('\n')
+        elif (player.total_value() > 21 or (dealer.total_value() > player.total_value()) or dealer.total_value() == 21) and (not blackjack):
+            print(f'\nDealer has {', '.join(dealer.display_hand())}\tTotal {dealer.total_value()}\nYou have {', '.join(player.display_hand())}\t  Total {player.total_value()}\n You lose!')
+            print('\n')
+        elif player.total_value() == dealer.total_value():
+            print(f'\nDealer has {', '.join(dealer.display_hand())}\tTotal {dealer.total_value()}\nYou have {', '.join(player.display_hand())}\t  Total {player.total_value()}\n push')
+            print('\n')
+        elif blackjack:
+            print(f'\nDealer has {', '.join(dealer.display_hand())}\tTotal {dealer.total_value()}\nYou have {', '.join(player.display_hand())}\t  Total {player.total_value()}\n Blackjack')
+            print('\n')
+        else:
+            print(f'\nDealer has {', '.join(dealer.display_hand())}\tTotal {dealer.total_value()}\nYou have {', '.join(player.display_hand())}\t  Total {player.total_value()}\n You win!')
+            print('\n')
+
+        player.clear()
+        dealer.clear()
 
 if __name__ == "__main__":
     play_blackjack()
