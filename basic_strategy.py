a1d1c5d3ac43473e38e741a016661c0dff603c9e
JeQuavionisTheIV <epersa@pcl.com>
2024-04-18 15:35:40 -0600

    well its breaking
    it's breaking but it's almost working, trying to figure out why

--- basic_strategy.py
+++ basic_strategy.py
@@ -1,5 +1,5 @@
-def basicStrategy():
-# dh      2,   3,   4,   5,   6,   7,   8,   9   10,  A
+def basicStrategy(handType, playerHand, dealerTotal):
+    # dh      2,   3,   4,   5,   6,   7,   8,   9   10,  A
     hard_basicStrategy = [
         ['H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H'], #4-8
         ['H', 'DH', 'DH', 'DH', 'DH', 'H', 'H', 'H', 'H', 'H'], #9
@@ -25,13 +25,92 @@ def basicStrategy():
     
 # dh      2,   3,   4,   5,   6,   7,   8,   9   10,  A
     splits = [
-        ['PH','PH','P','P','P','P','H','H','H','H']#2,2
-        ['PH','PH','P','P','P','P','H','H','H','H']#3,3
-        ['H','H','H','PH','PH','H','H','H','H','H']#4,4
-        ['DH','DH','DH','DH','DH','DH','DH','DH','H','H']#5,5
-        ['PH','P','P','P','P','H','H','H','H','H']#6,6
-        ['P','P','P','P','P','P','H','H','H','H']#7,7
-        ['P','P','P','P','P','P','P','P','P','P']#8,8
-        ['P','P','P','P','P','S','P','P','S','S']#9,9
-        ['P','P','P','P','P','P','P','P','P','P']#A,A
-    ]
+        ['PH', 'PH', 'P', 'P', 'P', 'P', 'H', 'H', 'H', 'H'], # 2,2
+        ['PH', 'PH', 'P', 'P', 'P', 'P', 'H', 'H', 'H', 'H'], # 3,3
+        ['H', 'H', 'H', 'PH', 'PH', 'H', 'H', 'H', 'H', 'H'], # 4,4
+        ['DH', 'DH', 'DH', 'DH', 'DH', 'DH', 'DH', 'DH', 'H', 'H'], # 5,5
+        ['PH', 'P', 'P', 'P', 'P', 'H', 'H', 'H', 'H', 'H'], # 6,6
+        ['P', 'P', 'P', 'P', 'P', 'P', 'H', 'H', 'H', 'H'], # 7,7
+        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'], # 8,8
+        ['P', 'P', 'P', 'P', 'P', 'S', 'P', 'P', 'S', 'S'], # 9,9
+        ['S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'], # 10,10
+        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'], # A,A
+        ]
+    col = get_col(dealerTotal)
+    row = get_row(handType, playerHand)
+
+    if handType == 'hard':
+        return hard_basicStrategy[row][col]
+    elif handType == 'soft':
+        return soft_basicStrategy[row][col]
+    else:
+        return splits[row][col]
+        
+
+
+def get_col(dealerTotal):
+    col_map = {'2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7, '10': 8, 'A': 9}
+    return col_map.get(str(dealerTotal), None)
+
+def get_row(handType, playerHand):
+    if handType == 'soft':
+        if playerHand.total_value() == 13:
+            row = 0
+        if playerHand.total_value() == 14:
+            row = 1
+        if playerHand.total_value() == 15:
+            row = 2
+        if playerHand.total_value() == 16:
+            row = 3
+        if playerHand.total_value() == 17:
+            row = 4
+        if playerHand.total_value() == 18:
+            row = 5
+        if playerHand.total_value() >= 19:
+            row = 6
+    elif handType == 'hard':
+        if playerHand.total_value() <= 8:
+            row = 0
+        if playerHand.total_value() == 9:
+            row = 1
+        if playerHand.total_value() == 10:
+            row = 2
+        if playerHand.total_value() == 11:
+            row = 3
+        if playerHand.total_value() == 12:
+            row = 4
+        if playerHand.total_value() == 13:
+            row = 5
+        if playerHand.total_value() == 14:
+            row = 6
+        if playerHand.total_value() == 15:
+            row = 7
+        if playerHand.total_value() == 16:
+            row = 8
+        if playerHand.total_value() >= 17:
+            row = 9
+    elif handType == 'split':
+        if playerHand[0][0].show_rank == '2':
+            row = 0
+        if playerHand[0][0].show_rank == '3':
+            row = 1
+        if playerHand[0][0].show_rank == '4':
+            row = 2
+        if playerHand[0][0].show_rank == '5':
+            row = 3
+        if playerHand[0][0].show_rank == '6':
+            row = 4
+        if playerHand[0][0].show_rank == '7':
+            row = 5
+        if playerHand[0][0].show_rank == '8':
+            row = 6
+        if playerHand[0][0].show_rank == '9':
+            row = 7
+        if playerHand[0][0].show_rank == '10':
+            row = 8
+        if playerHand[0][0].show_rank == 'A':
+            row = 9
+    return row
+    
+    return row_maps.get(handType, {}).get(total_value, None)
+
