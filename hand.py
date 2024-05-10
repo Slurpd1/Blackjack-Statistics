f0d5c47c3ab0f1e39922a8c6cccd9314982c6dd9
Slurpd1 <104402459+Slurpd1@users.noreply.github.com>
2024-05-10 13:13:04 -0700

    splits
    more work towards getting splits to work

--- hand.py
+++ hand.py
@@ -8,6 +8,9 @@ class Hand:
 
     # Other methods...
 
+    def can_split(self, num_of_splits):
+        return num_of_splits < 1 and self._hand[0].show_rank() == self._hand[1].show_rank()
+
     def get_card_at_index(self, index):
         """
         Get the card object at the specified index in the hand.
@@ -110,8 +113,8 @@ class PlayerHand:
             return True
         return False
 
-    def can_split(self):
-        return self.num_of_splits < 1 and self.hands[0].get_card_at_index(0).show_rank() == self.hands[0].get_card_at_index(1).show_rank()
+    def can_split(self, index=0):
+        return self.hands[index].can_split(self.num_of_splits)
 
     def add_card(self, card, hand_index=0):
         self.hands[hand_index].add_card(card)
